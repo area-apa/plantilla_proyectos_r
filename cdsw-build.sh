128c40ac81057bfcd098ba2ed3405295340bedda
area-apa <137091470+area-apa@users.noreply.github.com>
2023-07-27 14:18:55 -0400

    Estructura inicial
    primer commit

---
+++ cdsw-build.sh
@@ -0,0 +1,10 @@
+pip3 install scikit-learn
+pip3 install findspark
+pip3 install pyarrow
+pip3 install pandas
+if command -v pip2 >/dev/null 2>&1; then
+   pip2 install scikit-learn
+   pip2 install findspark
+   pip2 install pyarrow
+   pip2 install pandas
+fi
